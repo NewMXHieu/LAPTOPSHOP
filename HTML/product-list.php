d3f02783abde44aee0bc8750c0e544539e0a3eeb
Châu Thành <149070087+chauthanh258@users.noreply.github.com>
2024-03-31 17:12:45 +0700

    Thêm trang chủ, đăng ký, đăng nhập và sản phẩm

---
+++ HTML/product-list.php
@@ -0,0 +1,151 @@
+<?php
+include 'layout/header.php';
+?>
+<div class="wrapper">
+    <div class="content top-trangchu">
+        <div class="menu menu-left">
+            <div class="loc">
+                <h3>Hãng sản xuất</h3>
+                <div class="checkItems">
+                    <p><input type="checkbox" checked> Tất cả</p>
+                    <p><input type="checkbox"> Asus</p>
+                    <p><input type="checkbox"> Lenovo</p>
+                    <p><input type="checkbox"> MSI</p>
+                    <p><input type="checkbox"> Gigabyte</p>
+                    <p><input type="checkbox"> LG</p>
+                    <!-- <p><input type="checkbox"> Vaio</p> -->
+                    <p><input type="checkbox"> Apple (MacBook)</p>
+                    <p><input type="checkbox"> HP</p>
+                    <p><input type="checkbox"> Acer</p>
+                    <p><input type="checkbox"> Dell</p>
+                    <!-- <p><input type="checkbox"> Huawei</p> -->
+                    <!-- <p><input type="checkbox"> Masstel</p> -->
+                    <!-- <p><input type="checkbox"> Microsoft (Surface)</p> -->
+                </div>
+
+            </div>
+
+            <div class="loc">
+                <h3>Mức giá</h3>
+                <div class="checkItems">
+                    <p><input type="checkbox" checked> Tất cả</p>
+                    <p><input type="checkbox"> Dưới 10 triệu</p>
+                    <p><input type="checkbox"> Từ 10 - 15 triệu</p>
+                    <p><input type="checkbox"> Từ 15 - 20 triệu</p>
+                    <p><input type="checkbox"> Từ 20 - 25 triệu</p>
+                    <p><input type="checkbox"> Trên 25 triệu</p>
+                </div>
+            </div>
+
+            <div class="loc">
+                <h3>Màn hình</h3>
+                <div class="checkItems">
+                    <p><input type="checkbox" checked> Tất cả</p>
+                    <p><input type="checkbox"> Khoảng 13 inch</p>
+                    <p><input type="checkbox"> Khoảng 14 inch</p>
+                    <p><input type="checkbox"> Trên 15 inch</p>
+                </div>
+            </div>
+
+            <div class="loc">
+                <h3>CPU</h3>
+                <div class="checkItems">
+                    <p><input type="checkbox" checked> Tất cả</p>
+                    <p><input type="checkbox"> Intel Celeron</p>
+                    <p><input type="checkbox"> Intel Pentium</p>
+                    <p><input type="checkbox"> Intel Core i3</p>
+                    <p><input type="checkbox"> Intel Core i5</p>
+                    <p><input type="checkbox"> Intel Core i7</p>
+                    <p><input type="checkbox"> AMD Ryzen 3</p>
+                    <p><input type="checkbox"> AMD Ryzen 5</p>
+                    <p><input type="checkbox"> AMD Ryzen 7</p>
+                </div>
+            </div>
+
+            <div class="loc">
+                <h3>RAM</h3>
+                <div class="checkItems">
+                    <p><input type="checkbox" checked> Tất cả</p>
+                    <p><input type="checkbox"> 4GB</p>
+                    <p><input type="checkbox"> 8GB</p>
+                    <p><input type="checkbox"> 16GB</p>
+                    <p><input type="checkbox"> 32GB</p>
+                </div>
+            </div>
+
+            <div class="loc">
+                <h3>Card đồ họa</h3>
+                <div class="checkItems">
+                    <p title="Tất cả" for="check"><input type="checkbox" checked id="check"> Tất cả</p>
+                    <p><input type="checkbox"> NVIDIA GeForce Series</p>
+                    <p><input type="checkbox"> AMD Radeon Series</p>
+                    <p><input type="checkbox"> Card Onboard</p>
+                </div>
+            </div>
+
+            <div class="loc">
+                <h3>Ổ cứng</h3>
+                <div class="checkItems">
+                    <p><input type="checkbox" checked> Tất cả</p>
+                    <p><input type="checkbox"> SSD 1 TB</p>
+                    <p><input type="checkbox"> SSD 512 GB</p>
+                    <p><input type="checkbox"> SSD 256 GB</p>
+                    <p><input type="checkbox"> SSD 128 GB</p>
+                </div>
+            </div>
+
+            <div class="loc">
+                <h3>Tính năng đặc biệt</h3>
+                <div class="checkItems">
+                    <p><input type="checkbox" checked> Tất cả</p>
+                    <p><input type="checkbox"> CPU Intel</p>
+                    <p><input type="checkbox"> CPU Intel Gen 13</p>
+                    <p><input type="checkbox"> CPU AMD</p>
+                    <p><input type="checkbox"> Card NVIDIA RTX Series 4000</p>
+                    <p><input type="checkbox"> Sử dụng tấm nền IPS</p>
+                    <p><input type="checkbox"> Windows bản quyền</p>
+                    <p><input type="checkbox"> Mở khóa vân tay</p>
+                    <p><input type="checkbox"> Đèn nền bàn phím</p>
+                    <p><input type="checkbox"> Bảo vệ trẻ em</p>
+                </div>
+            </div>
+
+            <div class="loc">
+                <h3>Nhu cầu</h3>
+                <div class="checkItems">
+                    <p><input type="checkbox" checked> Tất cả</p>
+                    <p><input type="checkbox"> Gaming</p>
+                    <p><input type="checkbox"> Sinh viên - Văn phòng</p>
+                    <p><input type="checkbox"> Thiết kế đồ họa</p>
+                    <p><input type="checkbox"> Mỏng nhẹ</p>
+                    <p><input type="checkbox"> Doanh nhân</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="menu menu-right">
+            <input type="search" placeholder="Tim kiem...">
+            <input type="button" value="Kiem">
+            <input type="time">
+            <button>tim</button>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include 'layout/footer.php';
+?>
---
+++ HTML/trangchu.php
@@ -0,0 +1,48 @@
+<div class="wrapper">
+    <div class="content content-trangchu">
+        <div class="top-trangchu">
+            <div class="menu menu-left">
+                <ul class="list">
+                    <li><i class="fa-solid fa-laptop"></i> Laptop</li>
+                    <li><i class="fa-solid fa-gamepad"></i> Laptop Gaming</li>
+                    <li><i class="fa-solid fa-users-rectangle"></i> Laptop Sinh Viên - Văn Phòng</li>
+                    <li><i class="fa-solid fa-wand-magic-sparkles"></i> Laptop Thiết Kế Đồ Họa</li>
+                    <li>Laptop Mỏng Nhẹ</li>
+                    <li>Laptop Doanh Nhân</li>
+                </ul>
+            </div>
+            <div class="menu-right">
+                <?php
+                include 'layout/slider.php';
+                ?>
+            </div>
+        </div>
+
+        <div class="mid-trangchu">
+            <div class="show-sanpham">
+                <img alt="LAPTOP GAMING" src="https://lh3.googleusercontent.com/-E7ovjPHLnyWSk6S5Ew7aGiXOL2-8axqjI4H-SzYoCKQAYrP3YXh86tJ4wo61D4jD46LrV1M4HVzWChjExldrOkEDsOKkymA=w1232" width="100%" height="100%" class="show-sanpham-img">
+                <div class="top-show-sanpham">
+                    <a href="#">Laptop Gaming</a>
+                    <a href="#">Xem tất cả ></a>
+                </div>
+            </div>
+            <div class="show-sanpham">
+                <img alt="LAPTOP GAMING" src="https://lh3.googleusercontent.com/MX4EIYSDGrPytnzFOekQNgEScPD0aWg3kE0fnnN95fydTrvHOllvCBNrOEvF1sUO9BGK7AKCfxItTHHGjQVOE1yu3X_FwtXQ=w1232" width="100%" height="100%" class="show-sanpham-img">
+                <div class="top-show-sanpham">
+                    <a href="#">Laptop Gaming</a>
+                    <a href="#">Xem tất cả ></a>
+                </div>
+            </div>
+            <div class="show-sanpham">
+                <img alt="LAPTOP GAMING" src="https://lh3.googleusercontent.com/RslAPC8Xl3dhq1jfRSCUPm7w4WvIcPgbiA800QFEdcoEQFJnBPHy5AgMf2Rh00F3DavCin2xcjV5wx-mv-B5GH12w8jTlp1VHw=w1232" width="100%" height="100%" class="show-sanpham-img">
+                <div class="top-show-sanpham">
+                    <a href="#">Laptop Gaming</a>
+                    <a href="#">Xem tất cả ></a>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+</div>
+<script src="js/app.js"></script>
