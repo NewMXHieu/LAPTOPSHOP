e392aeaef770a82969eb000f648bd7e8a3f65740
Châu Thành <phamngocchauthanh258.com.vn@gmail.com>
2024-04-04 16:38:26 +0700

    Sửa folder

---
+++ SourceCode/TrangChu/register.php
@@ -0,0 +1,10 @@
+<div class="form-sign-up">
+    <form action="get">
+        <div class="sign-up">
+            <div class="header">Register</div>
+            <div class="name">
+                <input type="text">
+            </div>
+        </div>
+    </form>
+</div>
---
+++ SourceCode/TrangChu/trangchu.php
@@ -0,0 +1,189 @@
+<div class="wrapper">
+    <div class="content content-trangchu">
+        <div class="top-trangchu">
+            <div class="menu menu-left">
+                <ul class="list">
+                    <li><i class="fa-solid fa-laptop"></i> Laptop</li>
+                    <li><i class="fa-solid fa-gamepad"></i> Laptop Gaming</li>
+                    <li><i class="fa-solid fa-users-rectangle"></i> Laptop Sinh Viên - Văn Phòng</li>
+                    <li><i class="fa-solid fa-wand-magic-sparkles"></i> Laptop Thiết Kế Đồ Họa</li>
+                    <li>Laptop Mỏng Nhẹ</li>
+                    <li>Laptop Doanh Nhân</li>
+                </ul>
+            </div>
+            <div class="menu-right">
+                <?php
+                include 'slider.php';
+                ?>
+            </div>
+        </div>
+
+        <div class="mid-trangchu">
+            <div class="show-sanpham">
+                <img alt="LAPTOP GAMING" src="https://lh3.googleusercontent.com/-E7ovjPHLnyWSk6S5Ew7aGiXOL2-8axqjI4H-SzYoCKQAYrP3YXh86tJ4wo61D4jD46LrV1M4HVzWChjExldrOkEDsOKkymA=w1232" width="100%" height="100%" class="show-sanpham-img">
+                <div class="top-show-sanpham">
+                    <a href="#">Laptop Gaming</a>
+                    <a href="#">Xem tất cả ></a>
+                </div>
+                <div class="mid-show-sanpham">
+                    <div class="list-sanpham">
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                    </div>
+                    <div class="btns">
+                        <div class="btn-left btn"><i class='bx bx-chevron-left'></i></div>
+                        <div class="btn-right btn"><i class='bx bx-chevron-right'></i></div>
+                    </div>
+                </div>
+            </div>
+            <div class="show-sanpham">
+                <img alt="LAPTOP GAMING" src="https://lh3.googleusercontent.com/MX4EIYSDGrPytnzFOekQNgEScPD0aWg3kE0fnnN95fydTrvHOllvCBNrOEvF1sUO9BGK7AKCfxItTHHGjQVOE1yu3X_FwtXQ=w1232" width="100%" height="100%" class="show-sanpham-img">
+                <div class="top-show-sanpham">
+                    <a href="#">Laptop Gaming</a>
+                    <a href="#">Xem tất cả ></a>
+                </div>
+                <div class="mid-show-sanpham">
+                    <div class="list-sanpham">
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                    </div>
+                    <div class="btns">
+                        <div class="btn-left btn"><i class='bx bx-chevron-left'></i></div>
+                        <div class="btn-right btn"><i class='bx bx-chevron-right'></i></div>
+                    </div>
+                </div>
+            </div>
+            <div class="show-sanpham">
+                <img alt="LAPTOP GAMING" src="https://lh3.googleusercontent.com/RslAPC8Xl3dhq1jfRSCUPm7w4WvIcPgbiA800QFEdcoEQFJnBPHy5AgMf2Rh00F3DavCin2xcjV5wx-mv-B5GH12w8jTlp1VHw=w1232" width="100%" height="100%" class="show-sanpham-img">
+                <div class="top-show-sanpham">
+                    <a href="#">Laptop Gaming</a>
+                    <a href="#">Xem tất cả ></a>
+                </div>
+                <div class="mid-show-sanpham">
+                    <div class="list-sanpham">
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+                        <div class="product">
+                            <img src="../../image/10001.jpg" alt="">
+                            <h2>Laptop ACER</h2>
+                            <div class="price">2000Vnd</div>
+                            <button class="addCart">Thêm <i class="fa-solid fa-plus"></i></button>
+                        </div>
+
+                    </div>
+                    <div class="btns">
+                        <div class="btn-left btn"><i class='bx bx-chevron-left'></i></div>
+                        <div class="btn-right btn"><i class='bx bx-chevron-right'></i></div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+<script src="../../js/app.js"></script>
