d8bf38b2aa7afef3055f843d1264dfd18dc62aa8
QTLitSD <117745574+QTLitSD@users.noreply.github.com>
2024-05-18 00:25:22 +0700

    ...

--- src/laptopshop/footer.php
+++ src/laptopshop/footer.php
@@ -11,7 +11,7 @@
             <h4>Hỗ trợ khách hàng</h4>
             <p>Thẻ ưu đãi</p>
             <p>Hướng dẫn mua online</p>
-            <p>Ưu đãi</p>
+            <p>Ưu đãi mua hàng</p>
             <p>Chính sách trả góp</p>
             <p>Dịch vụ sửa chữa</p>
         </div>
@@ -24,20 +24,21 @@
         </div>
         <div class="information">
             <h4>Thông tin</h4>
-            <p>Giới thiệu</p>
+            <p>Giới thiệu về website</p>
             <p>Hệ thống cửa hàng</p>
-            <p>Hỏi đáp</p>
+            <p>Trung tâm bảo hành</p>
+            <p>Chính sách bảo mật</p>
             <p>Tuyển dụng</p>
         </div>
         <div class="social">
             <h4>Cộng đồng</h4>
-            <p>Gọi mua hàng ngay <a href="/">0123456789</a></p>
-            <p>Chăm sóc khách hàng <a href="/">0902123456</a></p>
+            <p>Gọi mua hàng ngay <a style="color:#0d6efd;" href="/">0123456789</a></p>
+            <p>Chăm sóc khách hàng <a style="color:#0d6efd;" href="/">0902123456</a></p>
         </div>
         <div class="contact">
             <h4>Liên hệ ngay</h4>
             <p>Hỗ trợ khách hàng</p>
-            <a href="/">cskh@gmail.com</a>
+            <a style="color:#0d6efd;" href="/">cskh@gmail.com</a>
             <p>Liên hệ báo giá</p>
         </div>
     </div>
